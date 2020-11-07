12ff5628239d7e339d086dfb0ad517c4e1dfd9b2
skahmed <skahmed@myamu.ac.in>
2020-11-07 14:47:37 +0530

    Form Validator event listener functions completed

---
+++ 01-FormValidator/script.js
@@ -0,0 +1,74 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
+
+
+const showError = (input, message) => {
+    const formControl = input.parentElement
+    formControl.className = 'form-control error'
+    const small = formControl.querySelector('small')
+    small.innerText = message
+}
+
+const showSuccess = (input) => {
+    const formControl = input.parentElement
+    formControl.className = 'form-control success'
+}
+
+const getFieldName = (input) => {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+const checkRequired = (inputArr) => {
+    inputArr.forEach(input => {
+        if (input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} is a required field`)
+        } else {
+            showSuccess(input)
+        }
+    });
+}
+
+const checkLength = (input, minLength, maxLength) => {
+    if (input.value.length < minLength) {
+        showError(input, `${getFieldName(input)} cannot be less than ${minLength} characters`)
+    } else if (input.value.length > maxLength) {
+        showError(input, `${getFieldName(input)} cannot be greater than ${minLength} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+const checkEmail = (input) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if (re.test(input.value.trim())){
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid')
+    }
+
+    // return re.test(String(input).toLowerCase())
+}
+
+
+const checkPasswordMatch = (input1, input2) => {
+
+    if (input1.value === input2.value && input1.value !== ""){
+        showSuccess(input1)
+        showSuccess(input2)
+    } else {
+        showError(input2, 'Passwords do not match')
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    checkRequired([username, email, password, password2])
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 15)
+    checkEmail(email)
+    checkPasswordMatch(password, password2)
+})
---
+++ README.md
@@ -0,0 +1,3 @@
+# Vanilla JS Practice Projects
+
+## 1. Form Validator
